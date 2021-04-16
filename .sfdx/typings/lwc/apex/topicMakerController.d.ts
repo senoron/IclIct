a6ae5c0e1d13b6c76608c129738097694429caff
senoron <perfectbynni@gmail.com>
2021-04-16 20:20:18 +0300

    Added topicMaker js + cls

---
+++ .sfdx/typings/lwc/apex/topicMakerController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/topicMakerController.getTeacherClasses" {
+  export default function getTeacherClasses(): Promise<any>;
+}
--- force-app/main/default/lwc/topicMaker/topicMaker.js
+++ force-app/main/default/lwc/topicMaker/topicMaker.js
@@ -1,5 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getTeacherClasses from '@salesforce/apex/topicMakerController.getTeacherClasses';
+
+
+
 
 export default class TopicMaker extends LightningElement {
-    
+    classes = [];
+    classOptions = [];
+    classValue = '';
+
+    subjects = [];
+    subjectOptions = [];
+    subjectValue = '';
+
+    topicTitle = '';
+
+    isSubjectDisabled = true;
+
+    @track topics = [];
+
+    async connectedCallback(){
+        this.classes = await getTeacherClasses();
+        //console.log(this.classes);
+        for (let i = 0; i < this.classes.length; i++) {
+            this.classOptions.push({label: this.classes[i].Grade__c + (isNaN(this.classes[i].Suffix__c) ? '' : this.classes[i].Suffix__c), value: this.classes[i].Id});
+        }
+        this.template.querySelector(".classesCombo").options = this.classOptions;
+    }
+
+    async classChange(event){
+        this.classValue = event.detail.value;
+    }
 }
