b866247279cd0e90e334374b7fd15db7270743aa
senoron <perfectbynni@gmail.com>
2021-04-16 19:39:23 +0300

    Added getClassSubject and getTeacherClassSubjects apex classes

--- .sfdx/typings/lwc/apex/classChooseController.d.ts
+++ .sfdx/typings/lwc/apex/classChooseController.d.ts
@@ -1,3 +1,9 @@
 declare module "@salesforce/apex/classChooseController.getAvialibleClasses" {
   export default function getAvialibleClasses(): Promise<any>;
 }
+declare module "@salesforce/apex/classChooseController.getClassSubjects" {
+  export default function getClassSubjects(param: {classId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/classChooseController.getTeacherClassSubjects" {
+  export default function getTeacherClassSubjects(param: {classId: any}): Promise<any>;
+}
