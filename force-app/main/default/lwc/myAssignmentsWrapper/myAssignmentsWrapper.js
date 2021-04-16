dfe2f7162e4966b984ed970150bed3861bca8071
YuriiLytvyn <yurii.lytvyn@redtag.com.ua>
2021-04-16 23:21:06 +0300

    POC assign

--- force-app/main/default/lwc/myAssignmentsWrapper/myAssignmentsWrapper.js
+++ force-app/main/default/lwc/myAssignmentsWrapper/myAssignmentsWrapper.js
@@ -1,16 +1,61 @@
-import { LightningElement } from 'lwc';
+import {
+    LightningElement,
+    track,
+    wire
+} from 'lwc';
 
 import getClass from '@salesforce/apex/MyAssignmentsController.getClass';
 import getSubjects from '@salesforce/apex/MyAssignmentsController.getSubjects';
 
 export default class MyAssignmentsWrapper extends LightningElement {
 
-    subjects;
+    @track subjects;
+    @track classRecordId;
 
-    async connectedCallback() {
-        const classRecord = await getClass();
-        this.subjects = await getSubjects({classId : classRecord.Id});
+    // @wire(getClass, {}) wiredClass({
+    //     data,
+    //     error
+    // }) {
+    //     this.classRecordId = data.Id;
+    // }
+
+    @wire(getSubjects, {
+        classId: '$classRecordId'
+    }) wiredSubjects({
+        data,
+        error
+    }) {
+        this.subjects = data;
     }
 
+    get subjectsList() {
+        try {
 
+            let result = [];
+
+            const keys = Object.keys(this.subjects);
+
+            for (let i = 0; i < keys.length; i++) {
+
+                const key = keys[i];
+
+                result.push({
+                    key,
+                    topics: this.subjects[key]
+                });
+
+            }
+
+            return result;
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    connectedCallback() {
+        getClass().then((result) => {
+            this.classRecordId = result.Id;
+        });
+    }
 }
