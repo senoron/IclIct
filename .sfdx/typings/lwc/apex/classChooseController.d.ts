1583ef1e19d4d9c13872190620d47f7c5b4ae202
YuriiLytvyn <yurii.lytvyn@redtag.com.ua>
2021-04-16 20:00:27 +0300

    footer logo

--- .sfdx/typings/lwc/apex/classChooseController.d.ts
+++ .sfdx/typings/lwc/apex/classChooseController.d.ts
@@ -1,9 +1,3 @@
 declare module "@salesforce/apex/classChooseController.getAvialibleClasses" {
   export default function getAvialibleClasses(): Promise<any>;
 }
-declare module "@salesforce/apex/classChooseController.getClassSubjects" {
-  export default function getClassSubjects(param: {classId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/classChooseController.getTeacherClassSubjects" {
-  export default function getTeacherClassSubjects(param: {classId: any}): Promise<any>;
-}
