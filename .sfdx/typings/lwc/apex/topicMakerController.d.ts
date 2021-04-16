d7d6d6fc3a92bd9ad060c498b37301d9b79386cb
senoron <perfectbynni@gmail.com>
2021-04-16 20:55:41 +0300

    Added full topicMaker HTML + JS + CLS

--- .sfdx/typings/lwc/apex/topicMakerController.d.ts
+++ .sfdx/typings/lwc/apex/topicMakerController.d.ts
@@ -1,3 +1,24 @@
 declare module "@salesforce/apex/topicMakerController.getTeacherClasses" {
   export default function getTeacherClasses(): Promise<any>;
 }
+declare module "@salesforce/apex/topicMakerController.getClassSubjects" {
+  export default function getClassSubjects(param: {classId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/topicMakerController.getSubjectTopics" {
+  export default function getSubjectTopics(param: {subjectId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/topicMakerController.addTopic" {
+  export default function addTopic(param: {subjectId: any, title: any}): Promise<any>;
+}
+declare module "@salesforce/apex/topicMakerController.deleteTopic" {
+  export default function deleteTopic(param: {topicId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/topicMakerController.getDocumentByTopic" {
+  export default function getDocumentByTopic(param: {topicId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/topicMakerController.deleteFile" {
+  export default function deleteFile(param: {documentId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/topicMakerController.getFileUrl" {
+  export default function getFileUrl(param: {documentId: any}): Promise<any>;
+}
--- force-app/main/default/lwc/topicMaker/topicMaker.js
+++ force-app/main/default/lwc/topicMaker/topicMaker.js
@@ -1,9 +1,12 @@
 import { LightningElement, track } from 'lwc';
 import getTeacherClasses from '@salesforce/apex/topicMakerController.getTeacherClasses';
-
-
-
-
+import getClassSubjects from '@salesforce/apex/topicMakerController.getClassSubjects';
+import addTopic from '@salesforce/apex/topicMakerController.addTopic';
+import getSubjectTopics from '@salesforce/apex/topicMakerController.getSubjectTopics';
+import deleteTopic from '@salesforce/apex/topicMakerController.deleteTopic';
+import getDocumentByTopic from '@salesforce/apex/topicMakerController.getDocumentByTopic';
+import deleteTopicFile from '@salesforce/apex/topicMakerController.deleteFile';
+import getFileUrl from '@salesforce/apex/topicMakerController.getFileUrl';
 export default class TopicMaker extends LightningElement {
     classes = [];
     classOptions = [];
@@ -30,5 +33,69 @@ export default class TopicMaker extends LightningElement {
 
     async classChange(event){
         this.classValue = event.detail.value;
+        this.subjects = [];
+        this.subjectOptions = [];
+        this.topics = [];
+        this.subjects = await getClassSubjects({classId: this.classValue});
+        for (let i = 0; i < this.subjects.length; i++) {
+            this.subjectOptions.push({label: this.subjects[i].Name, value: this.subjects[i].Id});
+        }
+        this.template.querySelector(".subjectsCombo").options = this.subjectOptions;
+        this.template.querySelector(".subjectsCombo").value = '';
+        this.isSubjectDisabled = false;
+    }
+
+    async updateTopics(){
+        this.topics = await getSubjectTopics({subjectId: this.subjectValue});
+        for (let i = 0; i < this.topics.length; i++) {
+            this.topics[i].files = await getDocumentByTopic({topicId: this.topics[i].Id});
+            //console.log(this.topics[i].files);
+        }
+        console.log(this.topics);
+    }
+    
+    async addNewTopic(event){
+        if(this.topicLabel !== ''){
+            await addTopic({subjectId: this.subjectValue, title: this.topicTitle})
+        }
+        this.template.querySelector(".topicInput").value = '';
+        this.topicTitle = '';
+        this.updateTopics();
+    }
+
+    changeTopicTitle(event){
+        this.topicTitle = event.detail.value;
+    }
+
+    subjectsChange(event){
+        this.subjectValue = event.detail.value;
+        this.updateTopics();
+    }
+
+    handleUploadFinished(event){
+        console.log('File uploaded');
+        this.updateTopics();
+    }
+
+    async navigateToFiles(event){
+        let url = await getFileUrl({documentId: event.currentTarget.value});
+        open(url, "_blank");
+    }
+
+    async deleteFile(event){
+        console.log(event.currentTarget.value);
+        await deleteTopicFile({documentId: event.currentTarget.value});
+        console.log("File deleted");
+        this.updateTopics();
+    }
+
+    get acceptedFormats() {
+        return ['.pdf', '.png', '.DOCX'];
+    }
+
+    async deleteTopic(event){
+        await deleteTopic({topicId: event.currentTarget.value});
+        this.topics = await getSubjectTopics({subjectId: this.subjectValue});
+        this.updateTopics();
     }
 }
