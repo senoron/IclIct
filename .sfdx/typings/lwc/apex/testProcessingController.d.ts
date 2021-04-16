935b4f16aa4bd0b78442c44b06e25b2dc4335a78
senoron <perfectbynni@gmail.com>
2021-04-17 01:31:34 +0300

    Added control works performing

---
+++ .sfdx/typings/lwc/apex/testProcessingController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/testProcessingController.getTest" {
+  export default function getTest(param: {testId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/testProcessingController.createStudentsHomework" {
+  export default function createStudentsHomework(param: {title: any, mark: any, testId: any}): Promise<any>;
+}
---
+++ force-app/main/default/lwc/testProcessing/testProcessing.js
@@ -0,0 +1,52 @@
+import { LightningElement, track } from 'lwc';
+import getTest from '@salesforce/apex/testProcessingController.getTest';
+import createStudentsHomework from '@salesforce/apex/testProcessingController.createStudentsHomework';
+export default class TestProcessing extends LightningElement {
+    testId =  '';
+    option = [];
+    @track tasks = [];
+    currentTest;
+
+    async shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    async connectedCallback(){
+        this.testId = URLSearchParams(location.search).get("testId");
+        this.currentTest = await getTest({testId: this.testId});
+        this.tasks = this.currentTest.Tasks__r;
+        await this.shuffle(this.tasks);
+        let tempArray = [];
+        //let tempRadio;
+        for(let i = 0; i < this.tasks.length; i++)
+        {
+            tempArray = [];
+            tempArray.push({label: this.tasks[i].Var1__c, value: this.tasks[i].Var1__c});
+            tempArray.push({label: this.tasks[i].Var2__c, value: this.tasks[i].Var2__c});
+            tempArray.push({label: this.tasks[i].Var3__c, value: this.tasks[i].Var3__c});
+            this.shuffle(tempArray);
+            this.template.querySelector('[data-id="' + this.tasks[i].Id + '"]').options = tempArray;
+        }   
+    }
+
+    async finishTest(event){
+        let mark = 0;
+        let maxMark = this.tasks.length;
+
+        for (let i = 0; i < this.tasks.length; i++) {
+            if(this.template.querySelector('[data-id="' + this.tasks[i].Id + '"]').value == this.tasks[i].Var1__c) mark++;
+        }
+
+        let finalMark = mark / maxMark * 12;
+        await createStudentsHomework({title: this.currentTest.Name, mark: finalMark, testId: this.currentTest.Id});
+        console.log("Closed");
+        open("/s/", "_self");
+    }
+}
